7302ead13287c8536bd40ebf28a48a21f5828d15
Rodnova Emiliya <emilirodinka@gmail.com>
2026-01-25 20:53:01 +0500

    added 4 more examples to practilceOne > python-basics > variables.py file

--- practicleOne/python-basics/variables.py
+++ practicleOne/python-basics/variables.py
@@ -1,4 +1,28 @@
+#example 1 from w3School
 x = 5
 y = "John"
 print(x)
 print(y)
+
+#example 2 from w3School
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#example 3 from w3School
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#example 5from w3School
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#example 5 from w3School
+
+x = "John"
+# is the same as
+x = 'John'
