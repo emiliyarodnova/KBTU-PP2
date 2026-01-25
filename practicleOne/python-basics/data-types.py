743ae5aaf923793f0028efbcb01570a67e4a1270
Rodnova Emiliya <emilirodinka@gmail.com>
2026-01-25 21:02:56 +0500

    added data-types.py ,  numbers.py , casting.py , strings.py files with examples

---
+++ practicleOne/python-basics/data-types.py
@@ -0,0 +1,16 @@
+#example 1 from w3School
+x = 5
+print(type(x))
+
+#example 2 from w3School
+x = "Hello World"
+print(type(x))
+#example 3 from w3School
+x = 1j
+print(type(x))
+#example 4 from w3School
+x = ("apple", "banana", "cherry")
+print(type(x))
+#example 5 from w3School
+x = True
+print(type(x))
---
+++ practicleOne/python-basics/strings.py
@@ -0,0 +1,23 @@
+#example 1 from w3School
+print("Hello")
+print('Hello')
+#example 2 from w3School
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#example 3 from w3School
+a = "Hello"
+print(a)
+#example 4 from w3School
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#example 5 from w3School
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
